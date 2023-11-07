48d91a9f101434b3b3b089b64aeae98abac6a408
Francesca Santoriello <f.santoriello@hotmail.com>
2023-11-07 12:28:48 +0100

    skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
+    <TerminalView />
 </template>
 
 <script>
+import TerminalView from './components/TerminalView.vue'
+
+export default {
+    components: {
+        TerminalView
+    }
+}
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200;400;600;700&display=swap');
+
+body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+}
+
+#app {
+    width: 100vw;
+    height: 100vh;
+    min-width: 100vw;
+    min-height: 100vh;
+    font-family: 'Roboto Mono', monospace;
+    background: #222333;
+    color: #EDF4F4;
+    padding: 2rem;
+}
 </style>
---
+++ src/components/TerminalView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="terminal">
+    </div>
+</template>
+
+<style scoped>
+#terminal {
+    width: 100vw;
+    height: 100vh;
+    min-width: 100vw;
+    min-height: 100vh;
+}
+</style>
